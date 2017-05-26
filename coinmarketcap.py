194408808bba85246d05a3bd01ce981d3605ce9a
Philipp Rouast <prouast@gmail.com>
2017-05-26 19:20:50 +1000

    Fixed JSON url

--- coinmarketcap.py
+++ coinmarketcap.py
@@ -42,16 +42,11 @@ def requestList(type, view):
 		baseUrl,
 		type,
 		view))
-		
+
 
 def requestMarketCap(slug):
 	"""Request market cap data for a given currency slug."""
-	#return _request("{0}/v1/datapoints/{1}/{2}/{3}/".format(
-	#	graphBaseUrl,
-	#	slug,
-	#	timestamp_0,
-	#	timestamp_1))
-	return _request("{0}/v1/datapoints/{1}/".format(
+	return _request("{0}/currencies/{1}/".format(
 		graphBaseUrl, slug))
 
 
@@ -59,23 +54,23 @@ def parseList(html, type):
     """Parse the information returned by requestList for view 'all'."""
     assert(type == "assets" or type == "currencies",
     	   "Can only parse assets or currencies")
-    
+
     data = []
 
     docRoot = lxml.html.fromstring(html)
     rows = docRoot.cssselect(
         "table#{0}-all > tbody > tr".format(type))
-        
+
     for row in rows:
         datum = {}
         fields = row.cssselect("td")
-        
+
         # Name and slug
         nameField = fields[1].cssselect("a")[0]
         datum['name'] = nameField.text_content().strip()
         datum['slug'] = nameField.attrib['href'].replace(
             '/{0}/'.format(type), '').replace('/', '').strip()
-		
+
         # Symbol
         datum['symbol'] = fields[2].text_content().strip()
 
@@ -95,7 +90,7 @@ def parseMarketCap(jsonDump, slug):
 	""" """
 	data = []
 	rawData = json.loads(jsonDump)
-		
+
 	# Covert data in document to wide format
 	dataIntermediate = {}
 	targetFields = [str(key.replace('_data', '')) for key in rawData.keys()]
@@ -105,22 +100,21 @@ def parseMarketCap(jsonDump, slug):
 			if time not in dataIntermediate:
 				dataIntermediate[time] = dict(zip(targetFields, [None]*len(targetFields)))
 			dataIntermediate[time][field] = row[1]
-		
+
 	# Generate derived data & alter format
 	times = sorted(dataIntermediate.keys())
 	for time in times:
 		datum = dataIntermediate[time]
 		datum['slug'] = slug
 		datum['time'] = datetime.utcfromtimestamp(time)
-		
-		if (datum['market_cap_by_available_supply'] is not None 
+
+		if (datum['market_cap_by_available_supply'] is not None
 			and datum['price_usd'] is not None
 			and datum['price_usd'] is not 0):
 			datum['est_available_supply'] = float(datum['market_cap_by_available_supply'] / datum['price_usd'])
 		else:
 			datum['est_available_supply'] = None
-		
+
 		data.append(datum)
-	
+
 	return data
-	
